69148a3953cf5014e8d6d9d0a96b86d51eea5bc1
Michael Kirk <michael.code@endoftheworl.de>
2020-12-28 17:03:25 -0800

    wip2

--- popdat/src/import_census.rs
+++ popdat/src/import_census.rs
@@ -9,7 +9,7 @@ impl CensusArea {
     pub fn fetch_all_for_map(map: &Map, timer: &mut Timer) -> anyhow::Result<Vec<CensusArea>> {
         timer.start("processing population areas fgb");
         let areas =
-            tokio::runtime::Runtime::new()?.block_on(Self::fetch_all_for_map_async(map, timer))?;
+            tokio::runtime::Builder::new_current_thread().build()?.block_on(Self::fetch_all_for_map_async(map, timer))?;
         timer.stop("processing population areas fgb");
         Ok(areas)
     }
